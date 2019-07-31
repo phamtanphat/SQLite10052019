fa24bf3d00b6f78016c5ea0940ce33c13aebe6a6
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-07-31 21:06:51 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/sqlite10052019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/sqlite10052019/MainActivity.java
@@ -6,9 +6,20 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    SQLite sqLite;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //Mỗi database sẽ có 1 file riêng
+        sqLite = new SQLite(this,"Quanlymonan.sql",null , 1);
+        //Tạo bảng : Thông tin món ăn
+//                    +Idmonan
+//                    +Ten
+//                    +Gia
+//                    +Dia chi
+        //Sử dụng ngôn ngữ truy vấn sql để thao tác với database
+        String createTable = "CREATE TABLE IF NOT EXISTS Monan (Id)";
     }
 }
