b59a9bfeb29fd89fde23ae5a0825f91d8df0981a
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-08-02 21:08:34 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/sqlite10052019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/sqlite10052019/MainActivity.java
@@ -1,21 +1,30 @@
 package khoapham.ptp.phamtanphat.sqlite10052019;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.database.Cursor;
 import android.os.Bundle;
 import android.util.Log;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     SQLite sqLite;
-
+    ArrayList<Monan> monanArrayList;
+    MonanAdapter monanAdapter;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerviewMonan);
         //Mỗi database sẽ có 1 file riêng
         sqLite = new SQLite(this,"Quanlymonan.sql",null , 1);
+        monanArrayList = new ArrayList<>();
+        monanAdapter  = new MonanAdapter(monanArrayList,MainActivity.this);
+        recyclerView.setAdapter(monanAdapter);
         //Tạo bảng : Thông tin món ăn
 //                    +Idmonan
 //                    +Ten
@@ -27,14 +36,16 @@ public class MainActivity extends AppCompatActivity {
 //        String insertData = "INSERT INTO Monan VALUES(null,'Cơm sườn',25000,'Quận 10')";
 //        sqLite.onQuery(insertData);
 
-//        String selectdatabase = "SELECT * FROM Monan";
-//        Cursor cursor = sqLite.getData(selectdatabase);
-//        while (cursor.moveToNext()){
-//            int id = cursor.getInt(0);
-//            String ten = cursor.getString(1);
-//            int gia = cursor.getInt(2);
-//            String diachi = cursor.getString(3);
-//        }
+        String selectdatabase = "SELECT * FROM Monan";
+        Cursor cursor = sqLite.getData(selectdatabase);
+        while (cursor.moveToNext()){
+            int id = cursor.getInt(0);
+            String ten = cursor.getString(1);
+            int gia = cursor.getInt(2);
+            String diachi = cursor.getString(3);
+            monanArrayList.add(new Monan(id,ten,gia,diachi));
+            monanAdapter.notifyDataSetChanged();
+        }
 
 
     }
