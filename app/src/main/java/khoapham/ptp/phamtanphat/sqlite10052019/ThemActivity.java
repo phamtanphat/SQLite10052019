2fdafbee473d8b59a9f1656ddfe75daee9902e95
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-08-05 20:35:12 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/sqlite10052019/ThemActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/sqlite10052019/ThemActivity.java
@@ -3,8 +3,15 @@ package khoapham.ptp.phamtanphat.sqlite10052019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.PatternMatcher;
+import android.text.TextUtils;
+import android.util.Patterns;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.util.regex.Pattern;
 
 public class ThemActivity extends AppCompatActivity {
 
@@ -20,5 +27,27 @@ public class ThemActivity extends AppCompatActivity {
         edtTen = findViewById(R.id.edittextTenmonan);
         btnHuy = findViewById(R.id.buttonHuy);
         btnXacnhan = findViewById(R.id.buttonXacnhan);
+
+        btnHuy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+        btnXacnhan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String ten = edtTen.getText().toString();
+                String gia = edtGia.getText().toString();
+                String diachi = edtDiachi.getText().toString();
+
+                if (!TextUtils.isEmpty(ten) && !TextUtils.isEmpty(gia) && !TextUtils.isEmpty(diachi)){
+                    String insert = "INSERT INTO Monan VALUES(null , '"+ten+"' , "+Integer.parseInt(gia)+" , '"+diachi+"')";
+                    SingletonDatabase.getInstance(ThemActivity.this).onQuery(insert);
+                }else {
+                    Toast.makeText(ThemActivity.this, "Truyen thieu thong tin", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
