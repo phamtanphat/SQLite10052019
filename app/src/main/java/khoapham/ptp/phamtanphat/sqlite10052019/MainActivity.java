dad86bd17a535057e6b98ae5816a210bb9554675
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-08-05 20:09:18 +0700

    activity Them

--- app/src/main/java/khoapham/ptp/phamtanphat/sqlite10052019/MainActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/sqlite10052019/MainActivity.java
@@ -67,7 +67,6 @@ public class MainActivity extends AppCompatActivity {
                 }else{
                     startActivity(intent);
                 }
-
                 break;
         }
         return super.onOptionsItemSelected(item);
