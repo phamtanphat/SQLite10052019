01eb00bb8350008b0dedf8e84d549a154635e0ca
Phạm Tấn Phát <phatdroid94@gmail.com>
2019-08-05 20:22:32 +0700

    a

--- app/src/main/java/khoapham/ptp/phamtanphat/sqlite10052019/ThemActivity.java
+++ app/src/main/java/khoapham/ptp/phamtanphat/sqlite10052019/ThemActivity.java
@@ -3,12 +3,22 @@ package khoapham.ptp.phamtanphat.sqlite10052019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ThemActivity extends AppCompatActivity {
 
+    EditText edtTen,edtGia,edtDiachi;
+    Button btnHuy,btnXacnhan;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_them);
+
+        edtDiachi = findViewById(R.id.edittextDiachi);
+        edtGia = findViewById(R.id.edittextGiamonan);
+        edtTen = findViewById(R.id.edittextTenmonan);
+        btnHuy = findViewById(R.id.buttonHuy);
+        btnXacnhan = findViewById(R.id.buttonXacnhan);
     }
 }
